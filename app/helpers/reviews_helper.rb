11c63299b650d69d03e56e9a6dad4ea8ffa113a0
ALRW <awerner1@googlemail.com>
2015-11-27 10:35:31 +0000

    Added star ratings helper

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,10 @@
 module ReviewsHelper
+  def star_rating(rating)
+    return rating unless rating.is_a?(Fixnum)
+    '★' * rating
+  end
+
+
+
+
 end
--- spec/helpers/reviews_helper_spec.rb
+++ spec/helpers/reviews_helper_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 def signup_signin
   visit('/')
@@ -30,4 +30,15 @@ def leave_review(thoughts, rating)
   fill_in 'Thoughts', with: thoughts
   select rating, from: 'Rating'
   click_button 'Leave Review'
+end
+
+describe ReviewsHelper, :type => :helper do
+  context '#star_rating' do
+    it 'does nothing for not a number' do
+      expect(helper.star_rating('N/A')).to eq('N/A')
+    end
+    it 'returns five black stars for a five' do
+      expect(helper.star_rating(5)).to eq('★★★★★')
+    end
+  end
 end
