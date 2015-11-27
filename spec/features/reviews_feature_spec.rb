d89cab29ee2837355b7cc534026a3dcc76461eb5
ALRW <awerner1@googlemail.com>
2015-11-27 10:10:03 +0000

    Added average rating method to restaurant class

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -24,5 +24,13 @@ feature 'reviewing' do
     expect(page).not_to have_content('This is amazing')
   end
 
-
+  scenario 'displays an average rating for all reviews' do
+    signup_signin
+    create_restaurant
+    leave_review('So so', '3')
+    click_link("Sign out")
+    signup_signin_user2
+    leave_review('Great', '5')
+    expect(page).to have_content('Average rating: 4')
+  end
 end
